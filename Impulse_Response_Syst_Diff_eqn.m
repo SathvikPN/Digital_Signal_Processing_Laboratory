5e2e4a709e5506c2cdeecb844dc7b5fb899fa8c3
Sathvik PN <66002135+SathvikPN@users.noreply.github.com>
2021-02-15 21:33:17 +0530

    Update Impulse_Response_Syst_Diff_eqn.m

--- Impulse_Response_Syst_Diff_eqn.m
+++ Impulse_Response_Syst_Diff_eqn.m
@@ -7,7 +7,11 @@ close all
 a = input("Enter co-eff numerator: ");
 b = input("Enter co-eff denominator: "); % 1
 n = input("No. samples: ");
+
 HI = impz(b,a,n);
+## using filter command
+## x = [1,zeros(1,n-1)];
+## HI = filter(b,a,x);
 
 t = 0: 1: (n-1) ;
 
