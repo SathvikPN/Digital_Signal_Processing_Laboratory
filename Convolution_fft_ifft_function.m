1d320e03cefa3ea96aa086b3e8fd3f62ad4012cc
Sathvik PN <66002135+SathvikPN@users.noreply.github.com>
2021-02-15 21:51:57 +0530

    Update and rename Linear_Convolution_using_builtin_fft_ifft_function.m to Convolution_fft_ifft_function.m

--- Convolution_fft_ifft_function.m
+++ Convolution_fft_ifft_function.m
@@ -4,8 +4,12 @@ close all;
 
 x = input("x seq: ");
 h = input("h seq: ");
+
 L1 = length(x);
 L2 = length(h);
+
+% LINEAR CONVOLUTION ==> N = (L1 + L2 - 1)
+% CIRCULAR CONVOLUTION   N = max(L1,L2)
 N = L1+L2-1;
 X = fft(x,N);
 H = fft(h,N);
